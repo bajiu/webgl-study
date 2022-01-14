28f62f47f4a73c47286cf11ed6819fa382f3150c
bajiu <bajiu@cashim.cn>
2022-01-14 17:20:42 +0800

    临时提交

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Router } from 'react-router';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <Router location={} navigator={}></Router> */}
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,11 @@
-export const routesConfig = {
+import { lazy } from 'react';
+import App from '../App';
 
-};
+export const routesConfig = [
+  {
+    path: '',
+    component: lazy(() => import('../App')),
+  },
+];
 
 export default routesConfig;
