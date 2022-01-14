d16e19373f0db91f7d83dba7276ec14dd80cea59
bajiu <bajiu@cashim.cn>
2022-01-14 10:17:28 +0800

    lint 完善

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    },
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'import/no-unresolved': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+      },
+    ],
+  },
+};
--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,8 @@
 export const getData = (): any => {
-    console.log('run this')
-    return {
-        a: 89
-    }
-}
+  console.log('run this');
+  return {
+    a: 89,
+  };
+};
+
+export default getData();
